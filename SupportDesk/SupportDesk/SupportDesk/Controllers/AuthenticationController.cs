5cce1249abc1d8e19b34f40217b61e8f77e9c521
Durgasaran-altrockstech <durgasaran.t@altrockstech.com>
2022-08-25 11:29:10 +0530

    Initial Commit
    Initial Commit - Only Theme Implemented

---
+++ SupportDesk/SupportDesk/SupportDesk/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SupportDesk.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [ActionName("SignIn")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("PasswordReset")]
+        public IActionResult PasswordResetBasic()
+        {
+            return View();
+        }
+
+        [ActionName("LockScreen")]
+        public IActionResult LockScreen()
+        {
+            return View();
+        }
+        [ActionName("Logout")]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ SupportDesk/SupportDesk/SupportDesk/Controllers/SupportTicketsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SupportDesk.Controllers
+{
+    public class SupportTicketsController : Controller
+    {
+        [ActionName("CreateTicket")]
+        public IActionResult CreateTicket()
+        {
+            return View();
+        }
+    }
+}
